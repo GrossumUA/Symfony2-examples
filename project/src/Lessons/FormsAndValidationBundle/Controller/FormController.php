f02f2373390290dceaeaf21294c3c856e96ea31c
Dmytro Sychevsky <dsychevsky@grossum.com>
2015-01-31 18:35:08 +0200

    addd validation and form templates

--- project/src/Lessons/FormsAndValidationBundle/Controller/FormController.php
+++ project/src/Lessons/FormsAndValidationBundle/Controller/FormController.php
@@ -3,42 +3,48 @@
 namespace Lessons\FormsAndValidationBundle\Controller;
 
 use Lessons\FormsAndValidationBundle\Entity\RequestContact;
+use Lessons\FormsAndValidationBundle\Form\RequestContactForm;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class FormController extends Controller
 {
 
-//    private $gender = array('m'=>1, 'f'=>2);
+    private $gender = array(1=>'Male', 2=>'Female');
 
-    public function requestAction()
+    public function requestAction(Request $request)
     {
 
+        $options['gender'] = $this->gender;
 
-//        $requestContactForm = $this->createForm(new RequestContactForm($options), new RequestContact());
-//
-//        $requestProjectForm->handleRequest($request);
-//
-//        if ($requestProjectForm->isValid()) {
-//
-//            $em = $this->getDoctrine()->getManager();
-//
-//            $requestProject = $requestProjectForm->getData();
-//
-//            $requestProject->setStatus(true);
-//
-//            $em->persist($requestProject);
-//            $em->flush();
-//
-//            $request->getSession()->getFlashBag()->add(
-//                'done',
-//                'Your request is saved!'
-//            );
-//
+
+        $requestContactForm = $this->createForm(new RequestContactForm($options), new RequestContact());
+
+        $requestContactForm->handleRequest($request);
+
+        if ($requestContactForm->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $requestContact = $requestContactForm->getData();
+
+            $em->persist($requestContact);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add(
+                'done',
+                'Your request is saved!'
+            );
+
 //            return $this->redirect($this->generateUrl('grossum_project'));
-//        }
+        }
 
 
+        return $this->render('FormsAndValidationBundle:Form:index.html.twig', array(
+            'request_contact_form' =>$requestContactForm->createView(),
 
-        return $this->render('FormsAndValidationBundle:Form:index.html.twig');
+        ));
+
     }
 }
--- project/src/Lessons/FormsAndValidationBundle/Form/RequestContactForm.php
+++ project/src/Lessons/FormsAndValidationBundle/Form/RequestContactForm.php
@@ -1,9 +1,12 @@
 <?php
 
-namespace Lessons\FormsAndValidationBundle;
+namespace Lessons\FormsAndValidationBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class RequestContactForm extends AbstractType
 {
@@ -20,15 +23,36 @@ class RequestContactForm extends AbstractType
     {
         $builder
             ->add('name', 'text', ['label' => '', 'trim' => true, 'required' => true ])
-            ->add('gender', 'textarea', ['label' => '', 'trim' => true, 'required' => true ])
             ->add('email', 'email', ['label' => '', 'trim' => true, 'required' => true ])
             ->add('message', 'textarea', ['label' => '', 'trim' => true, 'required' => true ])
-            ->add('needAnswer', 'textarea', ['label' => '', 'trim' => true, 'required' => true ])
 
             ->add('save', 'submit', array('label' => 'Send message'))
 
             ->setErrorBubbling(false)
         ;
+
+        //add custom field to form
+        $builder->add('needAnswer', 'choice', array(
+            'choices'   => array(1=>'Yes', 0=>'No'),
+            'data'      => 1,
+            'required'  => false,
+            'expanded'  => false,
+
+        ));
+
+        //add custom field to form
+        $builder->add('gender', 'choice', array(
+            'choices'   => $this->options['gender'],
+            'required'  => false,
+            'expanded'  =>true,
+        ));
+
+        //send email after submit
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'sendEmail'));
+
+    }
+
+    public function sendEmail($event){
 
     }
 
